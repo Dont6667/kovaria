a0e45da038d832185b73808a888f59bfc25bb321
minec <minec@DESKTOP-IER2JHM>
2024-10-19 14:29:33 +0700

    Add project files.

---
+++ kov/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace kov 
+{
+    class MyExeption : Exception 
+    {
+        public int ErrorWords { get; }
+        public MyExeption(string message) : base(message) { }
+
+      
+
+    }
+    class Programm 
+    {
+        static void Main() 
+        {
+            Exception[] exceptions = new Exception[]
+            {
+                new ArgumentException("Базовое исключение 1"),
+                new FormatException("Базовое исключение 2"),
+                new DivideByZeroException("Базовое исключение 3"),
+                new OverflowException("Базовое исключение 4"),
+                new MyExeption("Мое исключение")
+            };
+            foreach(var ex in exceptions)
+            try
+            {
+                    throw ex;
+            }
+            catch (Exception)
+            {
+                    Console.WriteLine($"Вызвано исключение{ex.Message}");
+            }
+             
+
+        }
+    }
+
+
+
+     
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
